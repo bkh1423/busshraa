0c0e9a18138d133aafd82629216f44efc53b8881
bkh1423 <bkh14231423@gmail.com>
2025-10-02 21:09:30 +0300

    تعديل

--- busshraa/urls.py
+++ busshraa/urls.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
-from django.urls import path, include  # نستورد include عشان نربط ملفات urls الخاصة بالتطبيقات
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-   
+    # لوحة التحكم الافتراضية
     path('admin/', admin.site.urls),
 
-    path('accounts/', include('accounts.urls')),   
-    path('shop/', include('shop.urls')),           
-    path('dashboard/', include('dashboard.urls')), 
+    # روابط التطبيقات
+    path('accounts/', include('accounts.urls')),
+    path('shop/', include('shop.urls')),
+    path('dashboard/', include('dashboard.urls')),
 ]
+
+# إضافة دعم عرض ملفات static و media أثناء التطوير
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
