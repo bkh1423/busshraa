4bd6dbda45ae0c3a95bfcbe27389f1f44e5d3240
bkh1423 <bkh14231423@gmail.com>
2025-10-05 00:23:37 +0300

    ااا

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from accounts.views import home   # استدعاء الدالة home من تطبيق accounts
 
-def accounts_home(request):
-    return render(request, "accounts/home.html")
+urlpatterns = [
+    path('', home, name='home'),   # الصفحة الرئيسية
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('shop/', include('shop.urls')),
+    path('dashboard/', include('dashboard.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
