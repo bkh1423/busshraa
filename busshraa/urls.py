6af3a6f13b1db26ae66ec70e1e0782b6a5a71a79
bkh1423 <bkh14231423@gmail.com>
2025-10-05 00:16:53 +0300

    تعديل الurls

--- busshraa/urls.py
+++ busshraa/urls.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from accounts.views import home   # استدعاء الدالة home من تطبيق accounts
+from accounts.views import accounts_home   # استدعاء الدالة بالاسم الصحيح
 
 urlpatterns = [
-    path('', home, name='home'),   # الصفحة الرئيسية
+    path('', accounts_home, name='home'),   # الصفحة الرئيسية
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('shop/', include('shop.urls')),
@@ -15,4 +15,5 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
