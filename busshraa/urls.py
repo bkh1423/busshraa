9a04c883ddfa426cb13dc5321b0b505b1dc8b627
bkh1423 <bkh14231423@gmail.com>
2025-10-04 23:23:15 +0300

    تعديل

--- busshraa/urls.py
+++ busshraa/urls.py
@@ -2,22 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views   # استدعاء ملف views.py اللي أضفناه في مجلد busshraa
+from accounts.views import home   # استدعاء الدالة home من تطبيق accounts
 
 urlpatterns = [
-    # الصفحة الرئيسية
-    path('', views.home, name='home'),
-
-    # لوحة التحكم الافتراضية
+    path('', home, name='home'),   # الصفحة الرئيسية
     path('admin/', admin.site.urls),
-
-    # روابط التطبيقات
     path('accounts/', include('accounts.urls')),
     path('shop/', include('shop.urls')),
     path('dashboard/', include('dashboard.urls')),
 ]
 
-# عرض ملفات static و media أثناء التطوير
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
